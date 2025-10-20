82a17a8def20477664dc166bf1662ed2d928093a
SiddhantaShrestha <siddhanta.shrestha2004@gmail.com>
2025-10-20 17:35:17 +0545

    message routes added

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -10,10 +10,10 @@ import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
 
-router.use(arcjetProtection);
+// router.use(arcjetProtection);
 
 router.post("/signup", signup);
-router.post("/login", arcjetProtection, login);
+router.post("/login", login);
 router.post("/logout", logout);
 
 router.put("/update-profile", protectedRoute, updateProfile);
