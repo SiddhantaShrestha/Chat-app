7968b43f8675b3455decb3f33bb13cc33732af95
SiddhantaShrestha <siddhanta.shrestha2004@gmail.com>
2025-10-22 08:47:02 +0545

    login and logout frontend

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import { useAuthStore } from "../store/useAuthStore";
 
 const ChatPage = () => {
-  return <div>ChatPage</div>;
+  const { logout } = useAuthStore();
+  return (
+    <div className="from-zinc-100">
+      ChatPage
+      <button onClick={logout}>Logout</button>
+    </div>
+  );
 };
 
 export default ChatPage;
